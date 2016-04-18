bdfac18b5ff32d1ccfda5582ea7929abebedbe8b
DJINA <pajkic.djina@gmail.com>
2016-04-18 14:23:09 -0400

    fixing sendSurveyModal with Rohan

--- src/containers/SurveyView/SurveyView.jsx
+++ src/containers/SurveyView/SurveyView.jsx
@@ -3,13 +3,13 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import Cookies from 'cookies-js';
-import moment from 'moment';
+
 
 // Styles
 import styles from '../UserList/userList.css';
 
 // Containers
-import { getSurveys, sendSurveyModal, sendSurvey } from '../../actions/surveyViewActions';
+import { getSurveys, toggleSurveyModal, sendSurvey } from '../../actions/surveyViewActions';
 import { getUsers } from '../../actions/userActions';
 
 
@@ -17,12 +17,14 @@ import { getUsers } from '../../actions/userActions';
 import Card from '../../components/Card';
 import Button from '../../components/Button';
 import SendSurveyModal from '../../components/SendSurveyModal';
+import SurveyViewItem from '../../components/SurveyViewItem';
 
 
 class SurveyView extends Component {
 
   state = {
-    chosenUser: this.props.chosenUser || {}
+    chosenUser: this.props.users[(this.props.users.length)-1] || {},
+    chosenSurvey: {}
   };
 
   componentWillMount() {
@@ -30,7 +32,29 @@ class SurveyView extends Component {
     this._renderUserList();
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (this.props.users.length > 0) {
+      const latestUserProps = nextProps.users[(nextProps.users.length)-1]
+      const latestUser = [latestUserProps.id, latestUserProps.first_name, latestUserProps.last_name, latestUserProps.email]
+      this._changeUserParams(latestUser);
+    }
+  }
+
   render() {
+    const surveyModal = Object.keys(this.state.chosenSurvey).length > 0
+    ? <SendSurveyModal 
+        surveyName={this.state.chosenSurvey.name} 
+        surveyID={this.state.chosenSurvey.id} 
+        users={this.props.users} 
+        showModal={true} 
+        toggleModal={this._renderSendSurveyModal} 
+        sendSurvey={this._sendSurvey} 
+        onChange={this._changeUserParams} 
+      />
+    : null;
+
+    const items = [...this.props.surveyList].map(val => <SurveyViewItem key={val.id} {...val} onShowModal={ this._selectSurveyForSending } />);
+
     return (
       <div className="userList">
         <Card>
@@ -47,42 +71,24 @@ class SurveyView extends Component {
                 <th>Last Modified</th>
                 <th> </th>
                 <th> </th>
-
-                <th/>
               </tr>
             </thead>
             <tbody>
-            {
-              [...this.props.surveyList]
-              .map(val => {
-                return (
-                  <tr key={val.id} className="userList-option">
-                    <td className="checkbox"><input type="checkbox" /></td>
-                    <td>{val.name} <a href="/survey" target="_blank">(Preview)</a></td>
-                    <td>{moment(val.created_at).fromNow()}</td>
-                    <td>{moment(val.updated_at).fromNow()}</td>
-                    <td>
-                      <Link to={`/dashboard/surveys/edit/${val.id}`}>
-                        <Button classes='inverse sm'>Edit</Button>
-                      </Link>
-                    </td>
-                    <td>
-                      <div className="userList-actionBar">
-                          <Button onClick={this._renderSendSurveyModal} classes="primary md">Send To User</Button>  
-                      </div>
-                      <SendSurveyModal surveyID={val.id} val={this.props.users} showModal={this.props.showModal} toggleModal={this._renderSendSurveyModal} sendSurvey={this._sendSurvey} onChange={this._changeUserParams} />
-                    </td>
-                  </tr>
-                );
-              })
-            }
+              { items }
             </tbody>
           </table>
         </Card>
-
+        { surveyModal }
       </div>
     );
   }
+
+  _selectSurveyForSending = (val) => {
+    const chosenSurvey = ([...this.props.surveyList].filter(item => item.id === val.id))[0]
+    this.setState({
+      chosenSurvey
+    })
+  };
 
   _renderSurveyList = () => {
     const { token, dispatch } = this.props;
@@ -94,48 +100,53 @@ class SurveyView extends Component {
     return dispatch(getUsers(token));
   };
 
-  // _renderPreSendSurvey = (survey) => {
-  //   this._renderSendSurveyModal();
-  //   this.setState({
-  //     chosenSurvey: survey
-  //   });
-  // }
-
   _renderSendSurveyModal = () => {
     const { token, dispatch } = this.props;
-    const { chosenUser } = this.state;
-    // this.setState({
-    //   newUser: {}
-    // });
-    return dispatch(sendSurveyModal());
+    return dispatch(toggleSurveyModal());
   };
 
   _sendSurvey = () => {
     this._renderSendSurveyModal();
     const { token, dispatch } = this.props;
     const { chosenUser } = this.state;
+
     return dispatch(sendSurvey(token, chosenUser))
   };
 
-  _changeUserParams = (value) => {
-    console.log("VALUEEEEEEEE", value, value.userId)
-    const userInfo = value.split(",");
-    console.log(userInfo)
+  _processValue = (value) => {
+    console.log("VALUE", value, typeof value)
+    let userInfo = null
+    if (typeof value == 'string') {
+      userInfo = value.split(",");
+      console.log(userInfo)
+      return userInfo
+    } else {
+      userInfo = value
+      console.log(userInfo)
+      return userInfo
+    }
+  }
 
+  _changeUserParams = (value) => {
+    console.log("VALUEEEEEEEE", value, typeof value)
+    const user = this._processValue(value);
+    console.log(user);
     const { chosenUser } = this.state;
-    this.setState({
-      chosenUser: {
-        userId: userInfo[0],
-        token: null,
-        firstName: userInfo[1],
-        lastName: userInfo[2],
-        email: userInfo[3],
-        companyName: "Scotia Bank",
-        surveyId: "1e9eddbc-7ede-43fd-9bde-364bba4d84e9",
-        emailTemplate: "welcomeEmail"
-      }
-    });
-  };
+    // this.setState({
+    //   chosenUser: {
+    //     userId: user[0],
+    //     token: null,
+    //     firstName: user[1],
+    //     lastName: user[2],
+    //     email: user[3],
+    //     companyName: "Scotiabank",
+    //     surveyId: user[4], 
+    //     // || "1e9eddbc-7ede-43fd-9bde-364bba4d84e9",
+    //     emailTemplate: "welcomeEmail"
+    //   }
+    // });
+    console.log(chosenUser)
+  }
 }
 
 function mapStateToProps(state) {
