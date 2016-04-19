2b39950218155e58a7e4e59f87e7088800322162
Rohan Nair <r@rohannair.ca>
2016-04-19 16:19:07 -0400

    Fixing newCont and newComp scripts

--- scripts/newContainer.js
+++ scripts/newContainer.js
@@ -10,7 +10,7 @@ if (!name) {
   process.exit(0);
 }
 
-const containerName   = _.compose(_.capitalize, _.camelCase)(name);
+const containerName   = _.flowRight(_.capitalize, _.camelCase)(name);
 const containerNameLC = _.camelCase(name);
 const dest            = path.join(__dirname, '..', 'src', 'containers', containerName);
 
