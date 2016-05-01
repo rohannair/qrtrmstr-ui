a05e0363df6f8f126710596f163e38bb33781929
Iordan Iordanov <iiordanov@gmail.com>
2016-04-30 21:55:10 -0400

    Adding a build script to use instead of sticking stuff directly into jenkins.

---
+++ scripts/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+umask 0002
+
+export PATH=/usr/local/bin:$PATH
+ARTIFACT_DIR=/opt/artifacts
+LATEST_FILE=qrtrmstr-ui-latest
+FILES_TO_INCLUDE="index.html public/"
+FILES_TO_DELETE="node_modules public"
+
+npm cache clean
+npm install
+npm run test
+
+npm run build
+
+ARTIFACT_FILE=${ARTIFACT_DIR}/qrtrmstr-ui-$(date +%Y-%m-%d_%H:%M:%S)-${GIT_COMMIT}.tar.bz2
+ARTIFACT_PATH=${ARTIFACT_DIR}/${ARTIFACT_FILE}
+
+# Pack and copy the artifact somewhere
+tar -cjvf ${ARTIFACT_FILE} ${FILES_TO_INCLUDE}
+
+# Tell deployment system what the latest artifact is:
+LATEST_FILE_TMP=$(mktemp)
+echo ${ARTIFACT_FILE} > ${LATEST_FILE_TMP}
+chmod ugo+r ${LATEST_FILE_TMP}
+mv ${LATEST_FILE_TMP} ${ARTIFACT_DIR}/${LATEST_FILE}
+
+rm -rf ${FILES_TO_DELETE}
+
+sudo /opt/ops/scripts/deploy.sh qrtrmstr-ui ${ARTIFACT_FILE}
