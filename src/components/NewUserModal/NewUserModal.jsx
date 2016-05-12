102e176ecf83a8112c98b6bfcdd088f2339c6514
DJINA <pajkic.djina@gmail.com>
2016-05-12 15:18:24 -0400

    fix ui styling

--- src/components/NewUserModal/NewUserModal.jsx
+++ src/components/NewUserModal/NewUserModal.jsx
@@ -11,7 +11,7 @@ class NewUserModal extends Component {
   render() {
     const {val, showModal, renderModal, submitNewUser, onChange, closeModal, loading, errorMessage, roles, chosenRole } = this.props;
     const loadingIcon = loading ? <i className="fa fa-cog fa-lg fa-spin spinner"></i> : null;
-    const errorText = errorMessage ? <div className="errorText"><i className="fa fa-exclamation-circle"></i><ul><li>{errorMessage}</li></ul></div> : null;
+    const errorText = errorMessage ? <div className="errorText"><p className="errorMsg">{errorMessage}</p></div> : null;
     const rolesOptions = Object.keys(roles).map(index => {
       let role = roles[index];
       return <option value={ JSON.stringify(Number(role.id)) } key={role.id}>{role.name}</option>;
@@ -20,72 +20,58 @@ class NewUserModal extends Component {
     return (
       <Modal animation={true} show={showModal} onHide={closeModal}>
         <Card className="modal">
-          <Modal.Header closeButton>
+          <Modal.Header>
             <Modal.Title>Add Team Member</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <div className="formField">
-              <div className="iconContainer">
-                <i className="fa fa-user iconInfo"></i>
-              </div>
+              <label>First Name: </label>
               <input
                 className="inputIcon"
                 name="first_name"
-                placeholder="First Name"
                 value= { val.first_name }
                 onChange={ e => onChange(e.target.name, e.target.value) }
               />
             </div>
             <div className="formField">
-              <div className="iconContainer">
-                <i className="fa fa-user iconInfo"></i>
-              </div>
+              <label>Last Name: </label>
               <input
                 className="inputIcon"
                 name="last_name"
-                placeholder="Last Name"
                 value= { val.last_name }
                 onChange={ e => onChange(e.target.name, e.target.value) }
               />
             </div>
             <div className="formField">
-              <div className="iconContainer">
-                <i className="fa fa-envelope iconInfo"></i>
-              </div>
+              <label>Email: </label>
               <input
                 className="inputIcon"
                 name="personal_email"
-                placeholder="Email Address"
                 value= { val.personal_email }
                 onChange={ e => onChange(e.target.name, e.target.value) }
               />
             </div>
             <div className="formField">
-              <div className="iconContainer select">
-                <i className="fa fa-envelope iconInfo"></i>
-              </div>
-              <select className="inputIcon selectInput" name="role_id" value={ chosenRole } onChange={e => onChange(e.target.name, JSON.parse(e.target.value)) }>
-                <option value="Choose A Role">Choose A Role</option><i className="fa fa-user iconInfo"></i>
+              <label>Role: </label>
+              <select className="inputIcon" name="role_id" value={ chosenRole } onChange={e => onChange(e.target.name, JSON.parse(e.target.value)) }>
+                <option value=""></option>
                 { rolesOptions }
               </select>
-              <div className="selectArrow">
-                <i className="fa fa-chevron-down"></i>
-              </div>
             </div>
 
           </Modal.Body>
           <Modal.Footer>
-            <div className="errorContainer">
-              { errorText }
-            </div>
             <div className="userButtonGroup">
               <ButtonGroup>
                 <div className="spinnerContainer">
                   { loadingIcon }
                 </div>
-                <Button classes="primary sm" onClick={submitNewUser}>Create New Member</Button>
-                <Button classes="primary sm" onClick={closeModal}>Cancel</Button>
+                <Button classes="inverse sm" onClick={closeModal}>Cancel</Button>
+                <Button classes="primary sm" onClick={submitNewUser}>Add</Button>
               </ButtonGroup>
+            </div>
+            <div className="errorContainer">
+              { errorText }
             </div>
           </Modal.Footer>
         </Card>
