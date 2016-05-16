970675a045836a2133678cc7e6ffd12b4ea95c2b
Rohan Nair <r@rohannair.ca>
2016-05-16 15:47:48 -0400

    Adding options to bio card, with internal state

--- src/components/SlideBio/SlideBio.jsx
+++ src/components/SlideBio/SlideBio.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import styles from './slideBio.css';
 
+import { capitalize } from 'lodash';
+
 import Button from '../Button';
 import ButtonGroup from '../ButtonGroup';
 import Card from '../Card';
@@ -11,12 +13,33 @@ class SlideBio extends Component {
   state = {
     heading: this.props.body.heading,
     desc: this.props.body.desc,
-    textAlign: this.props.body.textAlign || 'left'
+    textAlign: this.props.body.textAlign || 'left',
+    options: this.props.body.options || {}
   };
 
   render() {
     const { slide_number, id, onChange } = this.props;
-    const { heading, desc } = this.state;
+    const { heading, desc, options } = this.state;
+    let self = this;
+
+    const bioOptions = Object.keys(options).map(val => {
+      let label = val.split('_').join(' ');
+      let opt = options[val];
+      return (
+        <label key={val}>
+          <input
+            for={val}
+            type="checkbox"
+            checked={opt}
+            onChange={ self._updateSlideOption.bind(self, val) }
+          />
+          <span name={val} className="checkboxLabel">
+            { capitalize(label) }
+          </span>
+        </label>
+      );
+    });
+
     return (
       <div className="slideBio">
         <div className="slideEquipment">
@@ -28,10 +51,24 @@ class SlideBio extends Component {
               onChange={ e => this._updateBioState(e.target.name, e.target.value) }
             />
           </div>
+          <div className="options">
+            { bioOptions }
+          </div>
         </div>
-        <TextBox slideNum={ slide_number } body={ this.state } textAlign={ this.state.textAlign } bodyKey="desc" updateSlide={ this._updateBioState } />
       </div>
     );
+  };
+
+  _updateSlideOption = val => {
+    console.log('Updated', val);
+
+    this.setState({
+      ...this.state,
+      options: {
+        ...this.state.options,
+        [val]: !this.state.options[val]
+      }
+    });
   };
 
   _updateBioState = (key, value) => {
