3a156c143e6bdde8d2f410e1739ef0a8026ac965
Rohan Nair <rohannair@users.noreply.github.com>
2016-05-18 12:05:33 -0400

    DEV-79: Fixing playbook knowledge centre and bio cards (#26)

---
+++ src/components/PlaybookKnowledgeCentre/PlaybookKnowledgeCentre.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import styles from './playbookKnowledgeCentre.css';
+
+class PlaybookKnowledgeCentre extends Component {
+  state = {
+    selectedVideo: 0
+  };
+
+  render() {
+    const { body, heading } = this.props;
+    const { selectedVideo } = this.state;
+    const changeVideo = this._changeVideo;
+
+    const options = body.options.map((val, i) => {
+      const classes = i === selectedVideo ? 'active option' : 'option';
+      return (
+        <div className={ classes } key={val.id} onClick={ changeVideo.bind(self, i) }>
+          <i className="material-icons">ondemand_video</i>
+          { val.name }
+        </div>
+      );
+    });
+
+    return (
+      <div className="playbookKnowledgeCentre">
+        <h2>{ heading }</h2>
+        <p className = { body.textAlign || ''}>{ body.desc}</p>
+
+        <div className="playlist">
+          <div className="playlist-menu">
+            <div className="playlist-header">
+              <i className="material-icons">playlist_play</i>Videos
+            </div>
+            { options }
+          </div>
+
+          <div className="playlist-viewer">
+            <iframe src={`http://www.youtube.com/embed/${ body.options[selectedVideo].id}`} />
+          </div>
+        </div>
+        <div className="playlist-footer">
+          { body.footer}
+        </div>
+      </div>
+    );
+  };
+
+  _changeVideo = (idx) => this.setState({selectedVideo: idx});
+};
+
+export default PlaybookKnowledgeCentre;
