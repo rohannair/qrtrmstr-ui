205a9760924aacc4b46b32e92ea8aecfb26be467
Stosh Fabricius <stosh.fabricius@gmail.com>
2016-05-26 15:35:13 -0400

    Remove link in playbook checklist view

--- src/components/Global/Header.jsx
+++ src/components/Global/Header.jsx
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 const Header = props => {
+  const headerLink =  props.children  ? <Link to="/dashboard"> Quartermaster</Link> : <a href="#"> Quartermaster</a>
+
   return (
     <header className="appHeader">
       <div className="appHeader-section">
         <div className="appHeader-item appHeader-logo">
-          <Link to="/dashboard">
-            Quartermaster
-          </Link>
+          { headerLink }
         </div>
         <div className="appHeader-item">
           { props.children }
