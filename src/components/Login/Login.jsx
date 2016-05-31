3ba2d61d3558918056a0e79618465c0f18784b70
Rohan Nair <r@rohannair.ca>
2016-05-30 21:18:48 -0400

    DEV-160: Fixing login styling; adding error handling and making nicer

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import styles from './login.css';
 
 import Card from '../Card';
+import Button from '../Button';
 
 class Login extends Component {
   static propTypes = {
@@ -11,56 +12,88 @@ class Login extends Component {
 
   static defaultProps = {
     submitForm    : () => {},
-    forgotPassword: () => alert('OOPS')
+    forgotPassword: () => console.log('OOPS')
+  };
+
+  state = {
+    username: '',
+    password: '',
+    error: null
   };
 
   render() {
     const { submitForm, forgotPassword } = this.props;
+    const errorEl = this.props.error
+    ? <div>{`ERROR: ${this.props.error}`}</div>
+    : null;
 
     return (
       <div className="login-box">
-        <h1>Log In</h1>
-        <form
-        onSubmit={submitForm}>
+        <Card>
+          <h2>Log-in to Quartermaster</h2>
+          <form
+          className="form"
+          onSubmit={ this._submitForm }>
 
-          <label className="inputField">
-            Username:
-            <input
-              type="email"
-              ref="username"
-              name="username"
-              placeholder="Your email"
-              />
-          </label>
+            <label className="inputField">
+              Username:
+              <input
+                type="email"
+                ref="username"
+                name="username"
+                placeholder="Your email"
+                value={this.state.name}
+                onChange={ e => this._changeVal(e.target.name, e.target.value) }
+                />
+            </label>
 
-          <label className="inputField">
-            Password:
-            <input
-              type="password"
-              ref="password"
-              name="password"
-              placeholder="Your password"
-              />
-          </label>
+            <label className="inputField">
+              Password:
+              <input
+                type="password"
+                ref="password"
+                name="password"
+                placeholder="Your password"
+                value={this.state.password}
+                onChange={ e => this._changeVal(e.target.name, e.target.value) }
+                />
+            </label>
 
-          <div>
-            <button
-              className="button"
-              type="submit">
+            { errorEl }
+            <Button
+              classes="primary lgLong"
+              onClick={ this._submitForm }>
               Submit
-            </button>
+            </Button>
+          </form>
 
-          </div>
-        </form>
-
-        <button
-          className="button-link"
-          onClick={forgotPassword}>
-          I forgot my password
-        </button>
+          <Button
+            classes="transparent sm"
+            onClick={forgotPassword}>
+            I forgot my password
+          </Button>
+        </Card>
       </div>
     );
-  }
+  };
+
+  _changeVal = (name, val) => {
+    this.setState({
+      [name]: val
+    });
+  };
+
+  _submitForm = () => {
+    const { username, password } = this.state;
+    if (!username || !password) {
+      return this.setState({
+        error: 'Complete fields'
+      });
+    }
+
+    this.setState({ error: null });
+    return this.props.submitForm({username, password});
+  };
 }
 
 export default Login;
