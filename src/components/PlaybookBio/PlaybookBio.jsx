f796243f732e86a52779d0c073cfc693fd02574b
Djina Pajkic <pajkic.djina@gmail.com>
2016-06-08 11:02:42 -0400

    Dev 116 (DEV-134) - Users can submit cards that are part of the submitted_doc (#52)
    * users can now submit cards
    
    * fix submitted picture rendering in uploader and equipment options initally set to null
    
    * update submitted_doc with true or false submitted field
    
    * removing unused code
    
    * modify Uploader to update submitted_doc
    
    * remove extra imports and commented out code
    
    * move uploader to PlaybookCards and add componentWillMount to Uploader

--- src/components/PlaybookBio/PlaybookBio.jsx
+++ src/components/PlaybookBio/PlaybookBio.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './playbookBio.css';
+import Button from '../Button';
 
 import Uploader from '../../containers/Uploader';
 
 const PlaybookBio = (props) => {
+
+  const { slideKey } = props.findSlideKey(props.slide_number);
+  const currentBio = props.submittedDoc[slideKey].body.options.bio;
   const bodyOpts = props.body.options;
   const profilePic = bodyOpts.profile_image
   ? (<Uploader>
@@ -13,7 +17,7 @@ const PlaybookBio = (props) => {
   : null;
 
   const bio = bodyOpts.bio
-  ? <textarea placeholder="Tell the team a little bit about yourself..."/>
+  ? <textarea value={currentBio} name={props.slide_number} onChange={ e => props.onChange(e.target.name, 'bio', e.target.value) } placeholder="Tell the team a little bit about yourself..."/>
   : null;
 
   const facebook = bodyOpts.facebook
@@ -49,6 +53,9 @@ const PlaybookBio = (props) => {
           <h3>{ props.userInfo.firstName + ' ' + props.userInfo.lastName}</h3>
           { bio }
           { social }
+          <div className="slideFooter">
+            <Button classes="primary sm" onClick={props.onSubmit}>Submit</Button>
+          </div>
         </div>
       </div>
     </div>
